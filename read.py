84af701e7d52ba0da04c3e944d04aba8b5503139
yaseentp <yaseen.tpr@gmail.com>
2022-02-02 20:19:49 +0530

    added read and write logic

---
+++ read.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+
+
+def get_json_reader(BASE_DIR, table_name, chunksize=1000):
+    file_name = os.listdir(f'{BASE_DIR}/{table_name}')[0]
+    fp = f'{BASE_DIR}/{table_name}/{file_name}'
+    return pd.read_json(fp, lines=True, chunksize=chunksize)
+
+
+if __name__ == '__main__':
+    # This code will only run when we run the program.
+    # When we import, the program will not run.
+    BASE_DIR = os.environ.get('BASE_DIR')
+    table_name = os.environ.get('TABLE_NAME')
+    json_reader = get_json_reader(BASE_DIR, table_name)
+    for idx, df in enumerate(json_reader):
+        print(f'Number of records in chunk with index {idx} is {df.shape[0]}')
